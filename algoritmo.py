6bd3ee9b5f9f9703fd1224ca5dc89cce1948cd37
chaqui <josue.chaqui@gmail.com>
2016-05-05 02:27:51 -0600

    creado binario

--- algoritmo.py
+++ algoritmo.py
@@ -1,25 +1,44 @@
 # -*- coding: utf-8 -*-
+#Descripcion de los variables que tiene cada nodo
+import binario as binario
+
+
 class  CaracterCantidad():
 	"""docstring for  CaracterCantidad"""
 	def agregarValores(self,car,cant):
+		#caracter que almacena el nodo
 		self.caracter=car
+		#cantidad de caraccreres
 		self.cantidad=cant
-		
+
+
+class Retorno():
+
+	def agregarValores(self,nodo,caracter, binario):
+		self.nodo=nodo
+		self.caracter=caracter
+		self.binario=binario
+
+
 class Nodo():
 	"""docstring for nodo"""
+
 	def __init__(self):
 		self.bandera=0
 		self.visitado=False
+
 	def agregarHijoIzq(self,nodo):
 		self.nodoIzq=nodo
 		self.nodoIzq.bandera=1
+
 	def agregarHijoDer(self,nodo):
 		self.nodoDer=nodo
+
 	def AgregarContenido(self, cont):
 		self.contenido=cont
 
 
-			
+
 
 class Pila():
 	def crearLista(self):
@@ -40,12 +59,14 @@ class Pila():
 			self.lista.insert(0,nodo)
 		else:
 			self.lista[i].contenido.cantidad=self.lista[i].contenido.cantidad+1
+
 	def imprimirLista(self):
 			print "pila"
 			numero=0
 			for	elemento in self.lista:
 				print str(numero)+" "+chr(elemento.contenido.caracter)+" "+str(elemento.contenido.cantidad)
 				numero= numero + 1
+
 	def ordenar(self):
 		self.nuevaLista=[]
 		x=0
@@ -79,6 +100,7 @@ class Pila():
 							else:
 								y=y+1
 		self.lista=self.nuevaLista
+
 	def crearArbol1(self,listaX,iteracion):
 		print "iteracion "+str(iteracion)
 		if len(listaX)==1:
@@ -91,7 +113,11 @@ class Pila():
 			cont = CaracterCantidad()
 			cont.agregarValores("vacio", listaX[0].contenido.cantidad+listaX[1].contenido.cantidad)
 			nodo.AgregarContenido(cont)
-			print "caracter hijo Izq: "+str(listaX[0].contenido.caracter)
+			print nodo.bandera
+			try:
+				print "caracter hijo Izq: "+chr(listaX[0].contenido.caracter)
+			except:
+				print "caracter hijo Izq: " + str(listaX[0].contenido.caracter)
 			x=2
 			y=0
 			self.nuevaLista=[]
@@ -116,11 +142,8 @@ class Pila():
 								insertadoNodo=True
 							else:
 								if(listaX[len(listaX)-1].contenido.cantidad<nodo.contenido.cantidad):
-									print "q"
-									print len(listaX)
 									self.nuevaLista.insert(len(listaX),nodo)
 									insertadoNodo = True
-								print y
 								self.nuevaLista.insert(y,listaX[x])
 					else:
 						print "caracter"
@@ -128,37 +151,67 @@ class Pila():
 					y = y + 1
 					x = x + 1
 			return self.crearArbol1(self.nuevaLista,iteracion=iteracion+1)
+
 	def crearArbol(self):
 		print "crear arbol"
 		print str(len(self.lista))
 		self.Nlista=self.crearArbol1(self.lista,1)
 		self.lista=self.Nlista
-	def recorrerArbol(self,nodo,x):
+
+	def recorrerArbol(self,nodo,x,binario):
+		retorno=Retorno()
+		nNodo=nodo
+		hijoIzqVisitado=False
+		hijoDerVisitado=False
 		try:
+			print "try"
 			print str(x) +chr(nodo.contenido.caracter)+str(nodo.contenido.cantidad)
+			self.binario = binario + str(nodo.bandera)
+			print self.binario
 		except:
+			print "EXCEPT"
 			print str(x) + str(nodo.contenido.caracter) + str(nodo.contenido.cantidad)
+			self.binario = binario + str(nodo.bandera)
 		x=x+1
 		try:
-			print "izquierdo"
-			if(nodoizq.visitado==False):
-				self.recorrerArbol(nodo.nodoIzq,x)
+			if(nodo.nodoizq.visitado==False):
+				self.binario=binario+str(nodo.contenido.bandera)
+				print "izquierdo"
+				regreso= self.recorrerArbol(nodo.nodoIzq,x+1,self.binario)
+			else:
+				nNodo.hijoIzqVisitado=True
 			try:
-				if (nodoizq.visitado == False):
+				if(nodo.nodoDer.visitado==False):
 					print "derecho"
-					self.recorrerArbol(nodo.nodoDer, x)
-			except AttributeError, e:
-				return None
-		except AttributeError, e:
-			try:
+					regreso=self.recorrerArbol(nodo.nodoDer, x+1,self.binario)
+				else:
+					nNodo.hijoDerVisitado=True
+			except:
+				nNodo.hijoDerVisitado=True
+		except:
+			nNodo.hijoIzqVisitado=True
+
+		try:
+			if(nodo.nodoDer.visitado==False):
 				print "derecho"
-				self.recorrerArbol(nodo.nodoDer, x)
-			except AttributeError, e:
-				return None
+				self.retorno=self.recorrerArbol(nodo.nodoDer, x+1,self.binario)
+			else:
+				nNodo.hijoDerVisitado=True
+		except AttributeError, e:
+			nNodo.hijoDerVisitado=True
+		if(nodo.contenido.caracter!="vacio"):
+			retorno.caracter=nodo.contenido.caracter
+			nNodo.visitado=True
+			retorno.binario=self.binario
+		if (hijoDerVisitado == True and hijoIzqVisitado == True):
+			nNodo.visitado = True
+		retorno.nodo=nNodo
+		return retorno
+
+
 	def recorrerArbol1(self):
 		print "arbol"
-		#print str(self.lista[0].nodoIzq.contenido.caracter)
-		self.recorrerArbol(self.lista[0],1)
+		self.recorrerArbol(self.lista[0],1,"")
 
 class Algoritmo():
 	def crearPila(self):
